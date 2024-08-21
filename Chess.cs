38ebd7899e60a728ca091e139964dcded475f1d7
Hforna <henriqueflashpingo@gmail.com>
2024-08-21 04:29:57 -0300

    adding classes and MovePiece method

--- Chess.cs
+++ Chess.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.IO;
+
+interface IPiece
+{
+    public bool ValidPosition(int column, int row);
+}
+
+class Position
+{
+    public int column;
+    public int row;
+}
+
+class Board
+{
+    private readonly IPiece[,] Table = new IPiece[8, 8];
+
+    public void MovePiece(IPiece piece, char column, int row)
+    {
+        while (piece.ValidPosition(column, row) == false)
+        {
+            Console.WriteLine("Invalid move");
+            if (piece.ValidPosition(column, row))
+            {
+                Table[column, row] = piece;
+            }
+        }
+    }
+}
+
+class Game
+{
+
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        int[,] Table = new int[8, 8];
+        for(int i = 0; i < 8; i++)
+        {
+            for(int r = 0; r < 8; r++)
+            {
+                Console.Write(Table[i, r]);
+            }
+            Console.WriteLine();
+        }
+}
+}
