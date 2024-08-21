bd203582f7ecaa1fffab84b6f3042edc741cc682
Hforna <henriqueflashpingo@gmail.com>
2024-08-21 05:05:34 -0300

    add valid position for pieces

--- Chess.cs
+++ Chess.cs
@@ -4,29 +4,67 @@ using System.Globalization;
 using System.Text;
 using System.IO;
 
-interface IPiece
+enum Column
 {
-    public bool ValidPosition(int column, int row);
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H"
 }
 
-class Position
+class Piece
 {
-    public int column;
-    public int row;
+    public int currColumn;
+    public int currRow;
+    public bool ValidPosition(int ToColumn, int ToRow, int CurrColumn, int CurrRow, Piece[,] table)
+    {
+        return false;
+    }
+}
+
+class King : Piece
+{
+    public int currColumn;
+    public int currRow;
+
+    public bool ValidPosition(int ToColumn, int ToRow, int CurrColumn, int CurrRow, Piece[,] table)
+    {
+        int rowDiff = Math.Abs(ToRow - CurrRow);
+        int columnDiff = Math.Abs(ToColumn - CurrColumn);
+        bool veriP = rowDiff <= 1 && columnDiff <= 1;
+        if (table[ToRow, ToColumn] == null && veriP)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return "K";
+    }
+
 }
 
 class Board
 {
-    private readonly IPiece[,] Table = new IPiece[8, 8];
+    private readonly Piece[,] Table = new Piece[8, 8];
 
-    public void MovePiece(IPiece piece, char column, int row)
+    public void MovePiece(Piece piece, char ToColumn, int ToRow)
     {
-        while (piece.ValidPosition(column, row) == false)
+        while (piece.ValidPosition(ToColumn, ToRow, piece.currColumn, piece.currRow, Table) == false)
         {
             Console.WriteLine("Invalid move");
-            if (piece.ValidPosition(column, row))
+            if (piece.ValidPosition(ToColumn, ToRow, piece.currColumn, piece.currRow, Table))
             {
-                Table[column, row] = piece;
+                Table[ToRow, ToColumn] = piece;
+                piece.currColumn = ToColumn;
+                piece.currRow = ToRow;
+                break;
             }
         }
     }
@@ -43,13 +81,13 @@ class Program
     static void Main()
     {
         int[,] Table = new int[8, 8];
-        for(int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; i++)
         {
-            for(int r = 0; r < 8; r++)
+            for (int r = 0; r < 8; r++)
             {
                 Console.Write(Table[i, r]);
             }
             Console.WriteLine();
         }
-}
+    }
 }
